e5c7719c9509538a304ce1e517fce1ae04092038
Peter Chen <manchesterutd7@gmail.com>
2017-04-30 01:46:00 +0400

    finished Algorithm.js

--- public/js/algorithm/Algorithm.js
+++ public/js/algorithm/Algorithm.js
@@ -2,35 +2,20 @@
  * Created by Peter on 4/30/17.
  */
 
-// import java.util.ArrayList;
-// import java.util.Collections;
-// import java.util.HashMap;
-// import java.util.HashSet;
-// import java.util.LinkedList;
-// import java.util.List;
-// import java.util.Map;
-// import java.util.Set;
 
 // import Edge;
 // import Vertex;
 // import Graph;
 
 function DijkstraAlgorithm() {
-
     this.nodes, this.edges, this.settledNodes, this.unSettledNodes, this.predecessors, this.distance;
-    // private final List<Vertex> nodes;
-    // private final List<Edge> edges;
-    // private Set<Vertex> settledNodes;
-    // private Set<Vertex> unSettledNodes;
-    // private Map<Vertex, Vertex> predecessors;
-    // private Map<Vertex, Integer> distance;
 }
 
 DijkstraAlgorithm.prototype.execute = function(source) {
     this.settledNodes = [];
     this.unSettledNodes = [];
-    this.distance = { source: 0 };
-    this.predecessors = [];
+    this.distance[source] = 0;
+    this.predecessors = {};
     this.unSettledNodes.push(source);
     while (this.unSettledNodes.length > 0) {
         var node = this.getMinimum(this.unSettledNodes);
@@ -41,83 +26,92 @@ DijkstraAlgorithm.prototype.execute = function(source) {
         }
         this.findMinimalDistances(node);
     }
-}
+};
 
-DijkstraAlgorithm.prototype.findMinimalDistances = function(Vertex node) {
-    List<Vertex> adjacentNodes = getNeighbors(node);
-    for (Vertex target : adjacentNodes) {
-        if (getShortestDistance(target) > getShortestDistance(node) + getDistance(node, target)) {
-            distance.put(target, getShortestDistance(node) + getDistance(node, target));
-            predecessors.put(target, node);
-            unSettledNodes.add(target);
+DijkstraAlgorithm.prototype.findMinimalDistances = function(node) {
+    var adjacentNodes = this.getNeighbors(node);
+
+    for (var i = 0; i < adjacentNodes.length; i++) {
+        var target = adjacentNodes[i];
+        if (this.getShortestDistance(target) > this.getShortestDistance(node) + this.getDistance(node, target)) {
+            this.distance[target] = this.getShortestDistance(node) + this.getDistance(node, target);
+            this.unSettledNodes.push(target);
         }
     }
-}
+};
 
-DijkstraAlgorithm.prototype.getDistance = function(Vertex node, Vertex target) {
-    for (Edge edge : edges) {
+DijkstraAlgorithm.prototype.getDistance = function(node, target) {
+    for (var i = 0 ;i < this.edges.length; i++) {
+        var edge = edges[i];
         if (edge.getSource().equals(node) && edge.getDestination().equals(target)) {
             return edge.getWeight();
         }
     }
-    throw new RuntimeException("Should not happen");
-}
 
-DijkstraAlgorithm.prototype.getNeighbors = function(Vertex node) {
-    List<Vertex> neighbors = new ArrayList<Vertex>();
-    for (Edge edge : edges) {
-        if (edge.getSource().equals(node)
-            && !isSettled(edge.getDestination())) {
-            neighbors.add(edge.getDestination());
+    // throw new RuntimeException("Should not happen");
+};
+
+DijkstraAlgorithm.prototype.getNeighbors = function(node) {
+    var neighbors = [];
+
+    for (var i = 0 ;i < this.edges.length; i++) {
+        var edge = edges[i];
+        if (edge.getSource().equals(node) && !isSettled(edge.getDestination())) {
+            neighbors.push(edge.getDestination());
         }
     }
-    return neighbors;
-}
 
-DijkstraAlgorithm.prototype.getMinimum = function(Set<Vertex> vertexes) {
-    Vertex minimum = null;
-    for (Vertex vertex : vertexes) {
+    return neighbors;
+};
+
+DijkstraAlgorithm.prototype.getMinimum = function(vertexes) {
+    var minimum = null;
+
+    for (var i = 0; i < vertexes.length; i++) {
+        var vertex = vertexes[i];
         if (minimum == null) {
             minimum = vertex;
-        } else {
-            if (getShortestDistance(vertex) < getShortestDistance(minimum)) {
+        }
+        else {
+            if (this.getShortestDistance(vertex) < this.getShortestDistance(minimum)) {
                 minimum = vertex;
             }
         }
     }
+
     return minimum;
-}
+};
 
-DijkstraAlgorithm.prototype.isSettled = function(Vertex vertex) {
-    return settledNodes.contains(vertex);
-}
+DijkstraAlgorithm.prototype.isSettled = function(vertex) {
+    return this.settledNodes.contains(vertex);
+};
 
-DijkstraAlgorithm.prototype.getShortestDistance = function(Vertex destination) {
-    Integer d = distance.get(destination);
-    if (d == null) {
-        return Integer.MAX_VALUE;
+DijkstraAlgorithm.prototype.getShortestDistance = function(destination) {
+    var d = this.distance.indexOf(destination);
+    if (d == -1) {
+        return Number.MAX_VALUE;
     } else {
         return d;
     }
-}
+};
 
 /*
  * This method returns the path from the source to the selected target and
  * NULL if no path exists
  */
-DijkstraAlgorithm.prototype.getPath = function(Vertex target) {
-    LinkedList<Vertex> path = new LinkedList<Vertex>();
-    Vertex step = target;
+DijkstraAlgorithm.prototype.getPath = function(target) {
+    var path = [];
+    var step = target;
     // check if a path exists
-    if (predecessors.get(step) == null) {
+    if (this.predecessors.indexOf(step) == -1) {
         return null;
     }
-    path.add(step);
-    while (predecessors.get(step) != null) {
-        step = predecessors.get(step);
-        path.add(step);
+    path.push(step);
+    while (this.predecessors.indexOf(step) != -1) {
+        step = this.predecessors(this.predecessors.indexOf(step));
+        path.push(step);
     }
     // Put it into the correct order
-    Collections.reverse(path);
+    path.reverse();
     return path;
-}
+};

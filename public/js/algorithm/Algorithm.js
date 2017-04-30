d2c7edc3b260f60621a001f05a812882b50dfcdf
Peter Chen <manchesterutd7@gmail.com>
2017-04-30 11:56:26 +0400

    finished Java --> JS conversion

--- public/js/algorithm/Algorithm.js
+++ public/js/algorithm/Algorithm.js
@@ -3,12 +3,16 @@
  */
 
 
-// import Edge;
-// import Vertex;
-// import Graph;
-
+/*
+ * Algorithm class
+ */
 function DijkstraAlgorithm() {
-    this.nodes, this.edges, this.settledNodes, this.unSettledNodes, this.predecessors, this.distance;
+    this.nodes = [];
+    this.edges = [];
+    this.settledNodes = [];
+    this.unSettledNodes = [];
+    this.predecessors = {};
+    this.distance = {};
 }
 
 DijkstraAlgorithm.prototype.execute = function(source) {
@@ -69,7 +73,7 @@ DijkstraAlgorithm.prototype.getMinimum = function(vertexes) {
 
     for (var i = 0; i < vertexes.length; i++) {
         var vertex = vertexes[i];
-        if (minimum == null) {
+        if (minimum === null) {
             minimum = vertex;
         }
         else {
@@ -88,7 +92,7 @@ DijkstraAlgorithm.prototype.isSettled = function(vertex) {
 
 DijkstraAlgorithm.prototype.getShortestDistance = function(destination) {
     var d = this.distance.indexOf(destination);
-    if (d == -1) {
+    if (d === -1) {
         return Number.MAX_VALUE;
     } else {
         return d;
@@ -103,15 +107,109 @@ DijkstraAlgorithm.prototype.getPath = function(target) {
     var path = [];
     var step = target;
     // check if a path exists
-    if (this.predecessors.indexOf(step) == -1) {
+    if (this.predecessors.indexOf(step) === -1) {
         return null;
     }
     path.push(step);
-    while (this.predecessors.indexOf(step) != -1) {
+    while (this.predecessors.indexOf(step) !== -1) {
         step = this.predecessors(this.predecessors.indexOf(step));
         path.push(step);
     }
     // Put it into the correct order
     path.reverse();
     return path;
+};
+
+
+/*
+ * Edge class
+ */
+function Edge(id, source, destination, weight) {
+
+    this.id = id;
+    this.source = new Vertex();
+    this.destination = new Vertex();
+    this.weight = weight;
+}
+
+
+Edge.prototype.getId = function() {
+    return this.id;
+};
+
+Edge.prototype.getDestination = function() {
+    return this.destination;
+};
+
+Edge.prototype.getSource = function() {
+    return this.source;
+};
+
+Edge.prototype.getWeight = function() {
+    return this.weight;
+};
+
+Edge.prototype.toString = function() {
+    return this.source + " " + this.destination;
+};
+
+
+/*
+ * Graph class
+ */
+function Graph(vertexes, edges) {
+    this.vertexes = vertexes;
+    this.edges = edges;
+}
+
+Graph.prototype.getVertexes = function() {
+    return this.vertexes;
+};
+
+Graph.prototype.getEdges = function() {
+    return this.edges;
+};
+
+
+/*
+ * Vertex class
+ */
+function Vertex(id, name) {
+    this.id = id;
+    this.name = name;
+}
+
+Vertex.prototype.getId = function() {
+    return this.id;
+};
+
+Vertex.prototype.getName = function() {
+    return this.name;
+};
+
+/*Vertex.prototype.hashCode = function() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+};*/
+
+Vertex.prototype.equals = function(obj) {
+    if (this === obj) return true;
+    if (obj === null) return false;
+    if (!(this instanceof Vertex && obj instanceof Vertex)) return false;
+    var other = obj;
+    if (this.id === null) {
+        if (other.id !== null) {
+            return false;
+        }
+    }
+    else if (!this.id.equals(other.id)) {
+        return false;
+    }
+    return true;
+};
+
+Vertex.prototype.toString = function() {
+    return this.name;
 };
